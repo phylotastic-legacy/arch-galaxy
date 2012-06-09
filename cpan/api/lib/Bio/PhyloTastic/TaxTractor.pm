6c22ede24fcbc0863161d7e2283370db605526a2
Rutger Vos <rutgeraldo@gmail.com>
2012-06-09 18:01:21 +0200

    adding simple service to extract lists of taxa labels from various files

---
+++ cpan/api/lib/Bio/PhyloTastic/TaxTractor.pm
@@ -0,0 +1,66 @@
+package Bio::PhyloTastic::TaxTractor;
+use strict;
+use warnings;
+use Getopt::Long;
+use Bio::Phylo::IO 'parse';
+use Bio::Phylo::Util::Logger ':levels';
+
+sub run {
+		
+	# these are used as "perl compatible regular expressions" to process
+	# the labels, so you could strip out accession numbers and such
+	my $search = '';
+	my $replace = '';
+
+	# process command line arguments
+	my ( $infile, $format, $outfile );
+	my $verbosity = DEBUG;
+	GetOptions(
+		'infile=s'  => \$infile,
+		'format=s'  => \$format,
+		'search=s'  => \$search,
+		'replace=s' => \$replace,
+		'outfile=s' => \$outfile,
+		'verbose+'  => \$verbosity,
+	);
+	
+	# instantiate logger
+	my $log = Bio::Phylo::Util::Logger->new(
+		'-level' => $verbosity,
+		'-class' => 'main',
+	);
+	
+	# parse infile
+	my $project = parse(
+		'-format' => $format,
+		'-file'   => $infile,
+		'-as_project' => 1
+	);
+	
+	# compile hash set of distinct names, that are
+	# processed using s/$search/$replace/
+	my %names;
+	for my $block ( @{ $project->get_entities } ) {
+		my $taxa;
+		if ( $block->can('make_taxa') ) {
+			$taxa = $block->make_taxa;
+		}
+		else {
+			$taxa = $block;
+		}
+		$taxa->visit(sub{
+			my $name = shift->get_name;
+			$name =~ s/$search/$replace/g;
+			$names{$name} = 1
+		});
+	}
+	
+	# print output
+	{
+		open my $fh, '>', $outfile or die $!;
+		print $fh join "\n", keys %names;
+		close $fh;
+	}
+}
+
+1;
