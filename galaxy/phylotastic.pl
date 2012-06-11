48f5c7a5242896ed0b562bb871095c8fa35b0f04
Rutger Vos <rutgeraldo@gmail.com>
2012-06-11 13:13:04 +0200

    re-formatted error message

--- galaxy/phylotastic.pl
+++ galaxy/phylotastic.pl
@@ -13,12 +13,12 @@ eval { require $package };
 if ( $@ ) {
 	die <<"ERROR";
 ==================================== ERROR =====================================
-  FAILED TO LOAD $class
+ FAILED TO LOAD $class
 ================================= EXPLANATION ==================================
-  The perl package $class (or one of its dependencies) couldn't
-  be loaded correctly. This is either because it hasn't been installed
-  system-wide or because the environment variable PERL5LIB does not
-  include the path to the lib folder of the cpan api.
+ The perl package $class (or one of its dependencies) 
+ couldn't be loaded correctly. This is either because it hasn't been installed
+ system-wide or because the environment variable PERL5LIB does not include the
+ path to the lib folder of the cpan api.
 ================================================================================
   The error that was issued when attempting to run the $tool tool was:
 $@
